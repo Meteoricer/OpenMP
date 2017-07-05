293f909412ff28126bd711a77a9bba997ec5cb0d
Codelalala <yangchuxuan@gmail.com>
2017-07-05 10:12:58 -0400

    Update SaveAndLoad.cpp

--- SaveAndLoad.cpp
+++ SaveAndLoad.cpp
@@ -1,20 +1,36 @@
 template<typename Unit>
-int get_position(list<Unit> unit_list, typename list<Unit>::iterator unit_iter)
+int get_position(list<Unit> *unit_list, typename list<Unit>::iterator unit_iter)
 {
+	//cout << "i'm here" << endl;
+	//cout << "size:" << unit_list->size() << endl;
+	//system("pause");
 	int position = 0;
-	if (unit_list.size() != 0 && unit_iter != unit_list.end())
+	if (unit_list->size() != 0 && unit_iter != unit_list->end())
+		//if (false)
 	{
-		auto iter = unit_list.begin();
+		//cout << "lalala" << endl;
+		//system("pause");
+		auto iter = unit_list->begin();
 
 		while (iter != unit_iter)
 		{
+			//cout << "position:" << position << endl;
+			//system("pause");
+			/*if (position>1000)
+			{
+				cout << "comparison error" << endl;
+				system("pause");
+			}*/
 			position++;
 			iter++;
 		}
 	}
 	else
 	{
+		//cout << "lululu" << endl;
+		//system("pause");
 		position = -1;
 	}
+	//cout << "position:" << position << endl;
 	return position;
 };
