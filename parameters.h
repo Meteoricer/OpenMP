3b3142be0cfe6752ce4b4badee6ef1688def48f3
Codelalala <yangchuxuan@gmail.com>
2017-07-17 11:37:32 -0400

    Update parameters.h

--- parameters.h
+++ parameters.h
@@ -109,12 +109,12 @@ public:
 	double TT_fragmentation_rate = 0.001;//1;//8; #
 	double TD_fragmentation_rate = 0.01;//3;// 8;#
 	double DD_fragmentation_rate = 0.1;//5;//8;#5
-	double TT_depolymerize_minus_rate = 2;//1;//8;//80#1
-	double TD_depolymerize_minus_rate = TT_depolymerize_minus_rate * 40;//5;//20;//#5
-	double DD_depolymerize_minus_rate = TD_depolymerize_minus_rate * 40;//10;//40;//#5
-	double TT_depolymerize_plus_rate = TT_depolymerize_minus_rate / 40;//1;//8;//80#1
-	double TD_depolymerize_plus_rate = TT_depolymerize_plus_rate * 40;//5;//20;//#5
-	double DD_depolymerize_plus_rate = TD_depolymerize_plus_rate * 40;//10;//40;//#5
+	double TT_depolymerize_minus_rate = 1;//1;//8;//80#1
+	double TD_depolymerize_minus_rate = TT_depolymerize_minus_rate * 200;//5;//20;//#5
+	double DD_depolymerize_minus_rate = TD_depolymerize_minus_rate * 200;//10;//40;//#5
+	double TT_depolymerize_plus_rate = TT_depolymerize_minus_rate / 200;//1;//8;//80#1
+	double TD_depolymerize_plus_rate = TT_depolymerize_plus_rate * 200;//5;//20;//#5
+	double DD_depolymerize_plus_rate = TD_depolymerize_plus_rate * 200;//10;//40;//#5
 	double depolymerize_rate = 20;
 	
 	double attatch_rate = 400;//100;//100//0.000001;
