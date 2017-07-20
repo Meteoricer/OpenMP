59f1659e58e900c8fe7c42ea1040cf7b750a66b6
Codelalala <yangchuxuan@gmail.com>
2017-07-20 11:07:08 -0400

    Update parameters.h

--- parameters.h
+++ parameters.h
@@ -110,11 +110,11 @@ public:
 	double TD_fragmentation_rate = 0.01;//3;// 8;#
 	double DD_fragmentation_rate = 0.1;//5;//8;#5
 	double TT_depolymerize_minus_rate = 1;//1;//8;//80#1
-	double TD_depolymerize_minus_rate = TT_depolymerize_minus_rate * 40;//5;//20;//#5
-	double DD_depolymerize_minus_rate = TD_depolymerize_minus_rate * 40;//10;//40;//#5
-	double TT_depolymerize_plus_rate = TT_depolymerize_minus_rate / 40;//1;//8;//80#1
-	double TD_depolymerize_plus_rate = TT_depolymerize_plus_rate * 40;//5;//20;//#5
-	double DD_depolymerize_plus_rate = TD_depolymerize_plus_rate * 40;//10;//40;//#5
+	double TD_depolymerize_minus_rate = TT_depolymerize_minus_rate * 80;//5;//20;//#5
+	double DD_depolymerize_minus_rate = TD_depolymerize_minus_rate * 80;//10;//40;//#5
+	double TT_depolymerize_plus_rate = TT_depolymerize_minus_rate / 80;//1;//8;//80#1
+	double TD_depolymerize_plus_rate = TT_depolymerize_plus_rate * 80;//5;//20;//#5
+	double DD_depolymerize_plus_rate = TD_depolymerize_plus_rate * 80;//10;//40;//#5
 	double depolymerize_rate = 20;
 	
 	double attatch_rate = 400;//100;//100//0.000001;
